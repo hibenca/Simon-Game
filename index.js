bf7c83143777eb04a0285a311274c77e2f6df879
Corey <hiben06@gmail.com>
2020-04-19 10:57:19 -0500

    Added a button flash & sound function

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 
+
+
+
 function nextSequence() {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(50).fadeIn(50);
+    let buttonSound = new Audio("sounds/" + randomChosenColour + ".mp3");
+    // buttonSound.play();
 }
