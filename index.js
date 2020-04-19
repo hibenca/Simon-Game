45aeade637819932dc967e0c0433c0d68dd4bce7
Corey <hiben06@gmail.com>
2020-04-19 09:44:36 -0500

    Added a nextSequence() function to index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-$
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+}
