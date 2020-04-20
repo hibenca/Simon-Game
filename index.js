96d87f84e00859532ef10a73ef62588046b719de
Corey <hiben06@gmail.com>
2020-04-20 17:38:06 -0500

    Added sound function and working on a animation function

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Simon Game JavaScript file
 
+// Button colours
 let buttonColours = ["red", "blue", "green", "yellow"];
 
-// Store the game pattern
+// Game pattern
 let gamePattern = [];
 
+// Users pattern
+let userClickedPattern = [];
 
+// Game sequence
 function nextSequence() {
 
     // Choose a random button
@@ -16,12 +20,26 @@ function nextSequence() {
     // Animate button Flash
     $("#" + randomChosenColour).fadeOut(50).fadeIn(50);
 
-    // Play button sound
-    new Audio("sounds/" + randomChosenColour + ".mp3").play();
+    // Play sound
+    playSound(randomChosenColour);
 }
 
-// Handler function
+// User click sequence
 $('.btn').on("click", function () {
-    let userChosenColour = "notta"
-    console.log('test')
+    let userChosenColour = $(this).attr('id');
+    userClickedPattern.push(userChosenColour);
+
+    // Play sound
+    playSound(userChosenColour);
 });
+
+
+// Play button sound
+function playSound(name) {
+    new Audio("sounds/" + name + ".mp3").play();
+}
+
+// Animation... in progress
+function animatePress(currentColour) {
+    $('.btn').addClass("pressed").setTimeout($(".btn").removeClass("pressed"), 1100)
+}
