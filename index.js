27ed6e7d09d1206690f7a027edf0bf868249a36b
Corey <hiben06@gmail.com>
2020-04-20 07:13:03 -0500

    Added a handler function

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
+// Simon Game JavaScript file
+
 let buttonColours = ["red", "blue", "green", "yellow"];
 
+// Store the game pattern
 let gamePattern = [];
 
 
-
-
 function nextSequence() {
+
+    // Choose a random button
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
+    // Animate button Flash
     $("#" + randomChosenColour).fadeOut(50).fadeIn(50);
-    let buttonSound = new Audio("sounds/" + randomChosenColour + ".mp3");
-    // buttonSound.play();
+
+    // Play button sound
+    new Audio("sounds/" + randomChosenColour + ".mp3").play();
 }
+
+// Handler function
+$('.btn').on("click", function () {
+    let userChosenColour = "notta"
+    console.log('test')
+});
