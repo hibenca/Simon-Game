20db9a5e900e063ba354e7573c3e4cb77c0a4557
Corey <hiben06@gmail.com>
2020-04-22 10:13:30 -0500

    Finished Simon Game version 1.0

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Simon Game JavaScript file
 
+// Game level
+let level = 0
+
+// Game on/off
+let started = false;
+
 // Button colours
 let buttonColours = ["red", "blue", "green", "yellow"];
 
@@ -8,6 +14,14 @@ let gamePattern = [];
 
 // Users pattern
 let userClickedPattern = [];
+
+// Starting the game
+$(document).keydown(function (e) {
+    if (!started && e.key === 'a') {
+        nextSequence();
+        started = true;
+    }
+});
 
 // User click sequence
 $('.btn').on("click", function () {
@@ -19,16 +33,17 @@ $('.btn').on("click", function () {
 
     // Animation
     animatePress(userChosenColour);
+
+    // Check answer
+    checkAnswer(userClickedPattern.length - 1);
+
 });
-
-// Starting the game... in progress
-$(document).keypress(function (event) {
-    console.log(event.key)
-})
-
 
 // Game sequence
 function nextSequence() {
+
+    // reset click pattern
+    userClickedPattern = []
 
     // Choose a random button
     let randomNumber = Math.floor(Math.random() * 4);
@@ -40,6 +55,31 @@ function nextSequence() {
 
     // Play sound
     playSound(randomChosenColour);
+
+    // Level changer
+    level++;
+    $('h1').text("Level " + level)
+}
+
+// Check answer
+function checkAnswer(currentLevel) {
+    // If they get the answer right
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
+                nextSequence()
+            }, 1000);
+        }
+    // If they get the answer wrong
+    } else {
+        $('h1').text('Game Over, Press "a" key to start');
+        playSound("wrong");
+        $('body').addClass("game-over")
+        setTimeout(function () {
+            $('body').removeClass("game-over")
+        }, 200);
+        startOver();
+    }
 }
 
 // Play button sound
@@ -53,4 +93,12 @@ function animatePress(currentColour) {
     setTimeout(function() {
         $('#' + currentColour).removeClass("pressed")
     }, 100);
+}
+
+// Start over
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    $("h1").text('Press The "a" Key to Start Over');
+    started = false;
 }
